667416dd73c55356258d063b48db0c5ccffb5033
runnerdev1709 <121648224+runnerdev1709@users.noreply.github.com>
2025-03-04 19:33:13 -0300

    Create ex_001.py
    Testando com um Desafio  de Código, Curso de Python Mundo1 - Gustavo Guanabara.

---
+++ desafios_python/ex_001.py
@@ -0,0 +1,13 @@
+# 011 - faça um programa que peça a largura e altura de uma párede em metros, calcule sua area e a 
+# quantidade de tinta necessaria para pinta-la, sabendo que cada litro de tinta pinta
+#  uma area de 2M / Quadrado
+
+
+largura = float(input('Informe a Largura da parede: '))
+altura = float(input('Informe a Altura da parede: '))
+
+area = largura * altura     # pega os dados das variaveis calcula e atribui a area 
+print(f'sua parede possui {largura} x {altura} com a area total de {area}, metros')
+
+tinta = area / 2     # area dividido por 2 saberemos a qtdde de tinta que precisaremos de fato
+print(f'Uma parede com essa medida {area}, iremos precisar desta qdtte de tinta {tinta} !')
